f303d4d02ba4f58a4fbfcc2dd43e491049d797b1
est-amit-yadav <c-amit.yadav@bain.com>
2022-05-20 20:02:02 +0530

    Add project files.

---
+++ AsposePOC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Aspose.Slides;
+using Aspose.Slides.Export;
+using System.IO;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace AsposePOC
+{
+    class Program
+    {
+        private void SaveSlideToImage(ISlide sld)
+        {
+            Bitmap bmp = sld.GetThumbnail(1f, 1f);
+
+            // Save the image to disk in JPEG format
+            bmp.Save(string.Format(@"C:\Users\60102\source\repos\AsposePOC\AsposePOC\Images\Slide_{0}.jpg", sld.SlideNumber), System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+        static void Main(string[] args)
+        {
+            const string pathToVeryLargePresentationFile = @"C:\Users\60102\source\repos\AsposePOC\AsposePOC\pptx\Test.pptx";
+
+            LoadOptions loadOptions = new LoadOptions
+            {
+                BlobManagementOptions = {
+                    PresentationLockingBehavior = PresentationLockingBehavior.KeepLocked,
+                 }
+            };
+
+            using (Presentation pres = new Presentation(pathToVeryLargePresentationFile))
+            {
+                //OPTION 1
+                
+                //foreach (ISlide sld in pres.Slides)
+                //{
+                //    // Create a full scale image
+                //    Bitmap bmp = sld.GetThumbnail(1f, 1f);
+
+                //    // Save the image to disk in JPEG format
+                //    bmp.Save(string.Format(@"C:\Users\60102\source\repos\AsposePOC\AsposePOC\Images\Slide_{0}.jpg", sld.SlideNumber), System.Drawing.Imaging.ImageFormat.Jpeg);
+                //}
+
+                //OPTION 2 USING PTL
+                
+                Parallel.ForEach(pres.Slides, slide => new Program().SaveSlideToImage(slide));
+
+
+            }
+
+
+        }        
+    }
+}
